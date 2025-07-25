a900e1d425b27c5823aef4da280dca454cb9614a
“The <chatethakhun.jack@gmail.com>
2025-07-25 17:38:53 +0700

    Add login page with background and buttons

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,23 @@
 import { createFileRoute } from '@tanstack/react-router'
+import loginBg from '../assets/images/loginBG.png'
+import Button from '@/components/ui/Button'
 
 export const Route = createFileRoute('/')({
-  component: App,
+  component: RouteComponent,
 })
 
-function App() {
-  return <div className="text-center border h-full"></div>
+function RouteComponent() {
+  return (
+    <div
+      className="h-full p-3 pb-30"
+      style={{ backgroundImage: `url(${loginBg})` }}
+    >
+      <div className="flex flex-col items-center justify-end h-full gap-2 ">
+        <Button isBlock>Login</Button>
+        <Button isBlock secondary>
+          Register
+        </Button>
+      </div>
+    </div>
+  )
 }
