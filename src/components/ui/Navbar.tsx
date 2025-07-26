25f8fa21d28721f9d6deb450e33fca9a6a1ceb9f
“The <chatethakhun.jack@gmail.com>
2025-07-26 10:03:58 +0700

    Add navigation items for Profile and Settings

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -1,35 +1,60 @@
 import { BiMessageRoundedDetail } from 'react-icons/bi'
+import { FaRegUserCircle } from 'react-icons/fa'
+import { MdOutlineSettings } from 'react-icons/md'
 
 import useCustomRouter from '@/hooks/useCustomRouter'
 import { cn } from '@/utils/cn'
 
+const navs = [
+  {
+    name: 'Message',
+    path: '/chatapp',
+    id: 'message',
+    icon: BiMessageRoundedDetail,
+  },
+  {
+    name: 'Profile',
+    path: '/profile',
+    id: 'profile',
+    icon: FaRegUserCircle,
+  },
+  {
+    name: 'Setting',
+    path: '/setting',
+    id: 'setting',
+    icon: MdOutlineSettings,
+  },
+]
 const Navbar = () => {
   const { currentPathName, goTo } = useCustomRouter()
 
   return (
-    <div className="flex items-center justify-around w-full h-16 px-4 py-2 bg-white shadow-md absolute right-0 left-0 bottom-0">
-      <div className="flex items-center justify-around">
-        <div className="flex flex-col items-center ">
-          <button
-            className="flex items-center justify-center w-8 h-8 rounded-full bg-white cursor-pointer"
+    <div className="flex items-center justify-around w-full h-16 px-4 py-2 bg-white shadow-md absolute right-0 left-0 bottom-0 gap-2">
+      <div className="flex items-center justify-around gap-8">
+        {navs.map((nv) => (
+          <div
+            className="flex flex-col items-center cursor-pointer"
+            key={nv.id}
             onClick={() => {
-              goTo('/chatapp')
+              goTo(nv.path)
             }}
           >
-            <BiMessageRoundedDetail
-              className={cn('text-2xl text-gray-500', {
-                'text-primary': currentPathName === '/chatapp',
+            <button className="flex items-center justify-center w-8 h-8 rounded-full bg-white ">
+              <nv.icon
+                className={cn('text-2xl text-gray-500', {
+                  'text-primary': currentPathName === nv.path,
+                })}
+              />
+            </button>
+            <p
+              className={cn('text-dark', {
+                'text-primary font-bold': currentPathName === nv.path,
               })}
-            />
-          </button>
-          <p
-            className={cn({
-              'text-primary': currentPathName === '/chatapp',
-            })}
-          >
-            Message
-          </p>
-        </div>
+            >
+              {nv.name}
+            </p>
+          </div>
+        ))}
       </div>
     </div>
   )
