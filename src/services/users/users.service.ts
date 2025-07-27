5ace50dfb311b87f729679911142528cdd2162ea
“The <chatethakhun.jack@gmail.com>
2025-07-27 13:01:36 +0700

    Add auth headers to users API request

--- src/services/users/users.service.ts
+++ src/services/users/users.service.ts
@@ -5,7 +5,12 @@ export const getUsers = async () => {
     const response = await axios.get<{
       users: Array<User>
       unseenMessages: Record<string, number>
-    }>(`${import.meta.env.VITE_API_URL}/api/users/all`)
+    }>(`${import.meta.env.VITE_API_URL}/api/users/all`, {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `${localStorage.getItem('token')}`,
+      },
+    })
 
     return response.data
   } catch (error) {
