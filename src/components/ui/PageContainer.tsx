3c0b32ad3eb53a5e691a22e27dbd6c9ec2ea86a9
“The <chatethakhun.jack@gmail.com>
2025-09-19 15:24:40 +0700

    Add home button navigation to kits page container

--- src/components/ui/PageContainer.tsx
+++ src/components/ui/PageContainer.tsx
@@ -1,5 +1,6 @@
+import useCustomRouter from '@/hooks/useCustomRouter'
 import { cn } from '@/utils/cn'
-import { lazy, memo } from 'react'
+import { lazy, memo, useCallback } from 'react'
 
 const LanguageSwitcher = lazy(() => import('@/components/ui/LanguageSwitcher'))
 
@@ -8,6 +9,17 @@ interface PageContainerProps extends React.HTMLAttributes<HTMLDivElement> {
   children: React.ReactNode
 }
 const PageContainer = memo(({ children, noPadding }: PageContainerProps) => {
+  const { currentPathName, goTo } = useCustomRouter()
+
+  const isIncludeKitsPath = currentPathName.includes('kits')
+
+  const shouldShowHome = isIncludeKitsPath
+
+  const onClickToHome = useCallback(() => {
+    if (isIncludeKitsPath) {
+      goTo('/gunpla-kits/kits')
+    }
+  }, [isIncludeKitsPath])
   return (
     <>
       <div
@@ -19,7 +31,19 @@ const PageContainer = memo(({ children, noPadding }: PageContainerProps) => {
         )}
       >
         <div className="flex-1 flex flex-col gap-4">
-          <LanguageSwitcher />
+          <div className="flex justify-between">
+            {shouldShowHome && (
+              <div
+                className="cursor-pointer"
+                onClick={() => {
+                  onClickToHome()
+                }}
+              >
+                <h1 className="font-extrabold text-3xl">HOME</h1>
+              </div>
+            )}
+            <LanguageSwitcher />
+          </div>
           {children}
         </div>
       </div>
