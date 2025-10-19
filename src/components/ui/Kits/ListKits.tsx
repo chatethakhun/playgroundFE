1f16b4d5e78851398c38ac8b38650bb47a381570
“The <chatethakhun.jack@gmail.com>
2025-10-19 16:08:33 +0700

    Remove completion toggle; memoize RequireItem

--- src/components/ui/Kits/ListKits.tsx
+++ src/components/ui/Kits/ListKits.tsx
@@ -1,17 +1,11 @@
-import { updateIsFinished } from '@/services/gunplaKits/kit.service'
-import {
-  useMutation,
-  useQueryClient,
-  useSuspenseQuery,
-} from '@tanstack/react-query'
+import { useSuspenseQuery } from '@tanstack/react-query'
 import { memo } from 'react'
 import LoadingFullPage from '../LoadingFullPage'
 import ListItemContainer from '../ListItemContainer'
 import useCustomRouter from '@/hooks/useCustomRouter'
 import Button from '../Button'
 import { useTranslation } from 'react-i18next'
-import { Check, X } from 'lucide-react'
-import { cn } from '@/lib/utils'
+
 import NoData from '../NoData'
 import kitService from '@/services/v2/kit.service'
 
@@ -20,7 +14,6 @@ const KitItem = memo(
     kitName,
     grade,
     kitId,
-    isCompleted = false,
   }: {
     kitName: string
     grade: string
@@ -29,32 +22,7 @@ const KitItem = memo(
   }) => {
     const { goTo } = useCustomRouter()
     const { t } = useTranslation('kit')
-    const queryClient = useQueryClient()
 
-    const { mutate: toggleIsFinished } = useMutation({
-      mutationFn: () => updateIsFinished(kitId, !isCompleted),
-      onSuccess: (updatedKit) => {
-        queryClient.setQueryData<Array<Kit>>(
-          ['kits', isCompleted],
-          (oldData) => {
-            if (!oldData) return oldData
-
-            const newData = oldData.filter((kit) => kit._id !== updatedKit._id)
-            return newData
-          },
-        )
-
-        queryClient.setQueryData<Array<Kit>>(
-          ['kits', !isCompleted],
-          (oldData) => {
-            if (!oldData) return oldData
-
-            const newData = [...oldData, updatedKit]
-            return newData
-          },
-        )
-      },
-    })
     return (
       <ListItemContainer>
         <div>
@@ -64,22 +32,6 @@ const KitItem = memo(
           </span>
         </div>
         <div className="ml-auto flex gap-5 items-center">
-          {!isCompleted ? (
-            <Check
-              className={cn({
-                'text-green-500 w-6 h-6 cursor-pointer': true,
-              })}
-              onClick={() => toggleIsFinished()}
-            />
-          ) : (
-            <X
-              className={cn({
-                'text-red-500 w-6 h-6 cursor-pointer': true,
-              })}
-              onClick={() => toggleIsFinished()}
-            />
-          )}
-
           <Button onClick={() => goTo('/gunpla-kits/kits/' + kitId)}>
             {t('list.view-kit')}
           </Button>
--- src/components/ui/Kits/RequireItem.tsx
+++ src/components/ui/Kits/RequireItem.tsx
@@ -10,44 +10,47 @@ import { Badge } from '@/components/ui/badge'
 import RunnerColor from './RunnerColor'
 import MultipleColorBox from './MultipleColorBox'
 import { CheckCircle } from 'lucide-react'
+import { memo } from 'react'
 
-const RequireItem = ({
-  item,
-  onChecked,
-}: {
-  item: KitRequirementWithRunnerAndColor
-  onChecked: (item: KitRequirementWithRunnerAndColor) => void
-}) => {
-  return (
-    <Item className="border border-gray-600 rounded-2xl shadow-md">
-      <ItemHeader>
-        <div className="flex space-x-4">
-          {item.runner.color.is_multiple ? (
-            <MultipleColorBox />
-          ) : (
-            <RunnerColor color={item.runner.color.hex} />
-          )}
-          <ItemTitle>{item.runner.name}</ItemTitle>
-        </div>
-      </ItemHeader>
-      <ItemMedia />
-      <ItemContent>
-        <div className="flex flex-wrap space-x-2">
-          {item.gate.map((gate, index) => (
-            <Badge key={index} variant="outline" className="shadow-sm">
-              {gate}
-            </Badge>
-          ))}
-        </div>
-      </ItemContent>
-      <ItemActions>
-        <CheckCircle
-          onClick={() => onChecked(item)}
-          className={`${!item.is_cut ? 'text-gray-300' : 'text-green-500'} h-6 w-6`}
-        />
-      </ItemActions>
-    </Item>
-  )
-}
+const RequireItem = memo(
+  ({
+    item,
+    onChecked,
+  }: {
+    item: KitRequirementWithRunnerAndColor
+    onChecked: (item: KitRequirementWithRunnerAndColor) => void
+  }) => {
+    return (
+      <Item className="border border-gray-600 rounded-2xl shadow-md">
+        <ItemHeader>
+          <div className="flex space-x-4">
+            {item.runner.color.is_multiple ? (
+              <MultipleColorBox />
+            ) : (
+              <RunnerColor color={item.runner.color.hex} />
+            )}
+            <ItemTitle>{item.runner.name}</ItemTitle>
+          </div>
+        </ItemHeader>
+        <ItemMedia />
+        <ItemContent>
+          <div className="flex flex-wrap space-x-2">
+            {item.gate.map((gate, index) => (
+              <Badge key={index} variant="outline" className="shadow-sm">
+                {gate}
+              </Badge>
+            ))}
+          </div>
+        </ItemContent>
+        <ItemActions>
+          <CheckCircle
+            onClick={() => onChecked(item)}
+            className={`${!item.is_cut ? 'text-gray-300' : 'text-green-500'} h-6 w-6`}
+          />
+        </ItemActions>
+      </Item>
+    )
+  },
+)
 
 export default RequireItem
